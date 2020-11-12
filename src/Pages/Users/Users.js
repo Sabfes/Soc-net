4408835acc96e5f4e6e49c3f27050487791d02b9
Denis <jd.acemen@gmail.com>
2020-11-12 23:00:20 +0300

    53 less, add classes components

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -1,54 +1,66 @@
-import React from 'react'
+import React, {Component} from 'react';
 import classes from './Users.module.css'
 import {connect} from "react-redux";
 import UserCard from "./User/UserCard";
 import {showMoreCard, followToggle, setUsers} from "../../redux/actions/UsersActionCreators";
 import axios from 'axios'
 
-
-const Users = props => {
-
-
-    if (props.users.length === 0)  {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(res => {
-                console.log(res.data.items[0])
-                props.setUsers(res.data.items)
-            })
-            .catch(e => {
-                console.log(e)
-            })
-
+class Users extends Component {
+    state = {
+        getUsers: false,
     }
-    return (
-        <div className={classes.Users}>
-            <h1>users</h1>
 
-            <div className={classes.Users__container}>
-                {
-                    props.users.map( (u,i)=>{
-                        if (i<props.showUsers) {
-                            return (<UserCard
-                                key={i}
-                                id={u.id}
-                                name={u.name}
-                                status={u.status}
-                                isFollow={u.followed}
-                                imgUrl={u.photos.small}
-                                onClick={props.followToggle}
-                            />)
-                        } else {
-                            return null
-                        }
+    getUsers = () => {
+        if (this.props.users.length === 0)  {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(res => {
+                    console.log(res.data.items[0])
+                    this.props.setUsers(res.data.items)
+                })
+                .catch(e => {
+                    console.log(e)
+                })
+            this.setState({getUsers: !this.state.getUsers})
+        }
+    }
 
-                    })
-                }
+    render() {
+        return (
+            <div className={classes.Users}>
+                <h1>users</h1>
+
+                <div className={classes.Users__container}>
+                    {
+                        this.state.getUsers === false ? <button onClick={this.getUsers}>get users</button> : null
+                    }
+                    {
+                        this.props.users.map( (u,i)=>{
+                            if (i<this.props.showUsers) {
+                                return (<UserCard
+                                    key={i}
+                                    id={u.id}
+                                    name={u.name}
+                                    status={u.status}
+                                    isFollow={u.followed}
+                                    imgUrl={u.photos.small}
+                                    onClick={this.props.followToggle}
+                                />)
+                            } else {
+                                return null
+                            }
+
+                        })
+                    }
+                </div>
+
+                <button className={classes.Users__button} onClick={this.props.showMoreCard}>Show more</button>
             </div>
-
-            <button className={classes.Users__button} onClick={props.showMoreCard}>Show more</button>
-        </div>
-    )
+        )
+    }
 }
+
+
+
 
 function mapStateToProps(state) {
     return {
@@ -66,3 +78,7 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users)
+
+
+
+
--- src/redux/reducers/UsersReducer.js
+++ src/redux/reducers/UsersReducer.js
@@ -3,23 +3,7 @@
 import {FOLLOW_TOGGLE, SET_USERS, SHOW_MORE_CARD} from "../actions/ActionTypes";
 
 const initialState = {
-    users: [
-        // {id: 1, name: 'Dmitriy', location: {city: 'Moscow', country: 'Russia'}, status: 'hi i test desk', isFollow: false, },
-        // {id: 2, name: 'Leni', location: {city: 'Moscow', country: 'Russia'}, status: 'hi all', isFollow: false, },
-        // {id: 3, name: 'Ira', location: {city: 'Moscow', country: 'Russia'}, status: 'wwhaat', isFollow: true, },
-        // {id: 4, name: 'Dmitriy', location: {city: 'Moscow', country: 'Russia'}, status: 'hi i test desk', isFollow: false, },
-        // {id: 5, name: 'Leni', location: {city: 'Moscow', country: 'Russia'}, status: 'hi all', isFollow: false, },
-        // {id: 6, name: 'Ira', location: {city: 'Moscow', country: 'Russia'}, status: 'wwhaat', isFollow: true, },
-        // {id: 7, name: 'Dmitriy', location: {city: 'Moscow', country: 'Russia'}, status: 'hi i test desk', isFollow: false, },
-        // {id: 8, name: 'Leni', location: {city: 'Moscow', country: 'Russia'}, status: 'hi all', isFollow: false, },
-        // {id: 9, name: 'Ira', location: {city: 'Moscow', country: 'Russia'}, status: 'wwhaat', isFollow: true, },
-        // {id: 10, name: 'Dmitriy', location: {city: 'Moscow', country: 'Russia'}, status: 'hi i test desk', isFollow: false, },
-        // {id: 11, name: 'Leni', location: {city: 'Moscow', country: 'Russia'}, status: 'hi all', isFollow: false, },
-        // {id: 12, name: 'Ira', location: {city: 'Moscow', country: 'Russia'}, status: 'wwhaat', isFollow: true, },
-        // {id: 13, name: 'Dmitriy', location: {city: 'Moscow', country: 'Russia'}, status: 'hi i test desk', isFollow: false, },
-        // {id: 14, name: 'Leni', location: {city: 'Moscow', country: 'Russia'}, status: 'hi all', isFollow: false, },
-        // {id: 15, name: 'Ira', location: {city: 'Moscow', country: 'Russia'}, status: 'wwhaat', isFollow: true, },
-    ],
+    users: [],
     showUsers: 4,
 }
 
